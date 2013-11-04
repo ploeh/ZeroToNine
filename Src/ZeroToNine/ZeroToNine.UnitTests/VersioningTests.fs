8050be08a477f8f9ce71090676b2e4c1be3b1f02
Mark Seemann <mark@ploeh.dk>
2013-11-04 18:13:17 +0100

    Added IncrementVersion function
    in new Versioning module.

---
+++ Src/ZeroToNine/ZeroToNine.UnitTests/VersioningTests.fs
@@ -0,0 +1,41 @@
+﻿namespace Ploeh.ZeroToNine.UnitTests
+
+open System
+open Xunit
+open Xunit.Extensions
+
+module VersioningTests =
+    open Ploeh.ZeroToNine.Versioning
+
+    [<Theory>]
+    [<InlineData("1.0.0.0", Rank.Major,    "2.0.0.0")>]
+    [<InlineData("2.0.0.0", Rank.Major,    "3.0.0.0")>]
+    [<InlineData("2.1.0.0", Rank.Major,    "3.0.0.0")>]
+    [<InlineData("2.0.1.0", Rank.Major,    "3.0.0.0")>]
+    [<InlineData("2.0.0.1", Rank.Major,    "3.0.0.0")>]
+    [<InlineData("1.0.0.0", Rank.Minor,    "1.1.0.0")>]
+    [<InlineData("2.0.0.0", Rank.Minor,    "2.1.0.0")>]
+    [<InlineData("1.1.0.0", Rank.Minor,    "1.2.0.0")>]
+    [<InlineData("1.0.1.0", Rank.Minor,    "1.1.0.0")>]
+    [<InlineData("1.0.0.1", Rank.Minor,    "1.1.0.0")>]
+    [<InlineData("1.0.0.0", Rank.Build,    "1.0.1.0")>]
+    [<InlineData("1.0.1.0", Rank.Build,    "1.0.2.0")>]
+    [<InlineData("2.0.0.0", Rank.Build,    "2.0.1.0")>]
+    [<InlineData("1.1.0.0", Rank.Build,    "1.1.1.0")>]
+    [<InlineData("1.0.0.1", Rank.Build,    "1.0.1.0")>]
+    [<InlineData("1.0.0.0", Rank.Revision, "1.0.0.1")>]
+    [<InlineData("1.0.0.1", Rank.Revision, "1.0.0.2")>]
+    [<InlineData("2.0.0.0", Rank.Revision, "2.0.0.1")>]
+    [<InlineData("1.1.0.0", Rank.Revision, "1.1.0.1")>]
+    [<InlineData("1.0.1.0", Rank.Revision, "1.0.1.1")>]
+    let IncrementVersionReturnsCorrectResult
+        (version : string)
+        (rank : Rank)
+        (exp : string) =
+
+        let v = Version.Parse version
+        
+        let actual = IncrementVersion rank v
+
+        let expected = Version.Parse exp
+        Assert.Equal(expected, actual)
---
+++ Src/ZeroToNine/ZeroToNine/Versioning.fs
@@ -0,0 +1,22 @@
+﻿namespace Ploeh.ZeroToNine
+
+open System
+
+module Versioning =
+    type Rank =
+        | Major = 3
+        | Minor = 2
+        | Build = 1
+        | Revision = 0
+
+    let IncrementVersion rank (version : Version) =
+        match rank with
+        | Rank.Major ->
+            Version(version.Major + 1, 0, 0, 0)
+        | Rank.Minor ->
+            Version(version.Major, version.Minor + 1, 0, 0)
+        | Rank.Build ->
+            Version(version.Major, version.Minor, version.Build + 1, 0)
+        | _ ->
+            Version(version.Major, version.Minor, version.Build, version.Revision + 1)
+
