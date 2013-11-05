8ae8259f3eab03c2586fd616c44e4c7ae47e420f
Mark Seemann <mark@ploeh.dk>
2013-11-05 13:56:56 +0100

    Added an Args module with a Parse function.

---
+++ Src/ZeroToNine/Zero29.UnitTests/ArgsTests.fs
@@ -0,0 +1,16 @@
+﻿namespace Ploeh.ZeroToNine.UnitTests
+
+open Ploeh.ZeroToNine
+open Ploeh.ZeroToNine.Versioning
+open Xunit
+open Xunit.Extensions
+
+module ArgsTests =
+    [<Theory>]
+    [<InlineData("major",    Rank.Major)>]
+    [<InlineData("minor",    Rank.Minor)>]
+    [<InlineData("build",    Rank.Build)>]
+    [<InlineData("revision", Rank.Revision)>]
+    let ParseIncementReturnsCorrectResult(rank : string, expectedRank : Rank) =
+        let actual = [| "-i"; rank |] |> Args.Parse
+        Assert.Equal<Arg>([Increment(expectedRank)], actual |> Seq.toList)
---
+++ Src/ZeroToNine/Zero29/Args.fs
@@ -0,0 +1,16 @@
+﻿namespace Ploeh.ZeroToNine
+
+open Ploeh.ZeroToNine.Versioning
+
+type Arg =
+    | Increment of Rank
+
+module Args =
+    let Parse argv =
+        match argv |> Seq.toList with
+        | [_; "major"] -> Increment(Rank.Major)
+        | [_; "minor"] -> Increment(Rank.Minor)
+        | [_; "build"] -> Increment(Rank.Build)
+        | _ -> Increment(Rank.Revision)
+        |> Seq.singleton
+
