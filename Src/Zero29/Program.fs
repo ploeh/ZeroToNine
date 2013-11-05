d9dd2a4690606cbffc877e56dc4d2e75e11d77cc
Mark Seemann <mark@ploeh.dk>
2013-11-05 19:51:37 +0100

    Implemented main entry point.

--- Src/Zero29/Program.fs
+++ Src/Zero29/Program.fs
@@ -1,7 +1,27 @@
 ﻿namespace Ploeh.ZeroToNine
 
+open System
+open System.IO
+
 module Program =
+
+    let IncrementVersionsInFile rank file =
+        let writeAllLines file lines = File.WriteAllLines(file, lines)
+
+        File.ReadAllLines file
+        |> Array.map (Versioning.IncrementAssemblyAttribute rank)
+        |> writeAllLines file
+
+    let IncrementVersionsInAllAssemblyInfoFiles rank =
+        Directory.GetFiles(
+            Environment.CurrentDirectory,
+            "AssemblyInfo.*",
+            SearchOption.AllDirectories)
+        |> Array.iter (IncrementVersionsInFile rank)
 
     [<EntryPoint>]
     let main argv = 
+        match argv |> Args.Parse |> Seq.toList with
+        | [Increment(rank)] -> IncrementVersionsInAllAssemblyInfoFiles rank
+        | _ -> ()
         0 // return an integer exit code
