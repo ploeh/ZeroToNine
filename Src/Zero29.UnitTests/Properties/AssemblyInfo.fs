41268481169ff10e74fc4ddcdc5a8b09640af7c0
Mark Seemann <mark@ploeh.dk>
2013-11-06 16:06:16 +0100

    Added AssemblyInfo files to all projects.

---
+++ Src/Zero29.UnitTests/Properties/AssemblyInfo.fs
@@ -0,0 +1,43 @@
+﻿namespace Ploeh.ZeroToNine.UnitTests
+
+open System
+open System.Reflection
+open System.Resources
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("Zero29 unit tests")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("Ploeh")>]
+[<assembly: AssemblyProduct("Zero29")>]
+[<assembly: AssemblyCopyright("Copyright © Ploeh 2013")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("A03EEFEF-65E8-45EC-B28C-0705E0710909")>]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[<assembly: AssemblyVersion("0.1.0.0")>]
+[<assembly: AssemblyFileVersion("0.1.0.0")>]
+
+[<assembly: NeutralResourcesLanguage("en")>]
+
+()
