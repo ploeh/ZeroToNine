edebb1d67a246adba0d20832c01ea8d36724fa32
Mark Seemann <mark@ploeh.dk>
2013-12-02 21:51:11 +0100

    Added GetRelativePath function.

---
+++ Src/ZeroToNine.UnitTests/FileSystemTests.fs
@@ -0,0 +1,25 @@
+﻿namespace Ploeh.ZeroToNine.UnitTests
+
+open System
+open Xunit
+open Xunit.Extensions
+
+module FileSystemTests =
+    open Ploeh.ZeroToNine.FileSystem
+    
+    [<Theory>]
+    [<InlineData(@"C:\foo", @"C:\foo\bar", @".\bar")>]
+    [<InlineData(@"C:\foo", @"C:\foo\baz", @".\baz")>]
+    [<InlineData(@"C:\ploeh", @"C:\ploeh\fnaah", @".\fnaah")>]
+    [<InlineData(@"C:\ploeh\", @"C:\ploeh\fnaah", @".\fnaah")>]
+    [<InlineData(@"C:\ploeh", @"C:\foo\bar", @"C:\foo\bar")>]
+    [<InlineData(@"C:\foo\baz", @"C:\foo\grault", @"C:\foo\grault")>]
+    [<InlineData(@"C:\foo\bar", @"C:\foo\bar\baz\qux.corge", @".\baz\qux.corge")>]
+    let GetRelativePathReturnsCorrectResult
+        (workingDirectory : string)
+        (path : string)
+        (expected : string) =
+
+        let actual = GetRelativePath workingDirectory path
+        Assert.Equal<string>(expected, actual)
+
---
+++ Src/ZeroToNine/FileSystem.fs
@@ -0,0 +1,13 @@
+﻿namespace Ploeh.ZeroToNine
+
+open System.IO
+
+module FileSystem =
+    let GetRelativePath (workingDirectory : string) (path : string) =
+        match path.IndexOf workingDirectory with
+        | 0 ->
+            let wdLength =
+                workingDirectory.TrimEnd(Path.DirectorySeparatorChar).Length
+            "." + path.Substring wdLength
+        | _ -> path
+
