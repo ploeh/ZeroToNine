65a18bb81128922a5e8d5744ccd865d7739ac31e
Marco Rasp <marco.rasp@googlemail.com>
2013-12-12 21:05:31 +0100

    implement version list

--- Src/Zero29/Args.fs
+++ Src/Zero29/Args.fs
@@ -4,10 +4,12 @@ open Ploeh.ZeroToNine.Versioning
 
 type Arg =
     | Increment of Rank
+    | ListVersions 
 
 module Args =
     let Parse argv =
         match argv |> Seq.toList with
+        | ["-l"] -> ListVersions
         | [_; "major"] -> Increment(Rank.Major)
         | [_; "minor"] -> Increment(Rank.Minor)
         | [_; "build"] -> Increment(Rank.Build)
--- Src/Zero29/Program.fs
+++ Src/Zero29/Program.fs
@@ -33,6 +33,24 @@ module Program =
         |> Array.map parse
         |> writeTo file
 
+    let ListVersionsInFile file =
+        let parse text =
+            match Versioning.TryParse text with
+            | None -> None
+            | Some(pv) ->
+                sprintf "%O %s %O"
+                        (file |> FileSystem.GetRelativePath Environment.CurrentDirectory)
+                        (pv.AttributeType.Name.Replace("Attribute", ""))
+                        pv.Version
+                |> Some
+
+        let printn (s : string) = Console.WriteLine s
+
+        File.ReadAllLines file
+        |> Array.map parse
+        |> Array.choose id 
+        |> Array.iter printn
+
     let IncrementVersionsInAllAssemblyInfoFiles rank =
         Directory.GetFiles(
             Environment.CurrentDirectory,
@@ -40,9 +58,17 @@ module Program =
             SearchOption.AllDirectories)
         |> Array.iter (IncrementVersionsInFile rank)
 
+    let ListVersionsInAllAssemblyInfoFiles =
+        Directory.GetFiles(
+            Environment.CurrentDirectory,
+            "AssemblyInfo.*",
+            SearchOption.AllDirectories)
+        |> Array.iter ListVersionsInFile
+
     [<EntryPoint>]
     let main argv = 
         match argv |> Args.Parse |> Seq.toList with
         | [Increment(rank)] -> IncrementVersionsInAllAssemblyInfoFiles rank
+        | [ListVersions] -> ListVersionsInAllAssemblyInfoFiles
         | _ -> ()
         0 // return an integer exit code
